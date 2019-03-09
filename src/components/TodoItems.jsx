47dae83067398a1dea1eb4f5ad1e9d8cb77dcdda
Prabeen <prabeensoreng2119@gmail.com>
2019-03-09 21:02:40 +0530

    designing list

---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TodoItem() {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p>Checkbox Placeholder</p>
+      <br />
+    </div>
+  );
+}
+
+export default TodoItem;
